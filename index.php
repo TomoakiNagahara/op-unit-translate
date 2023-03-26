6076f00722c6cfd10a471ecf06e91023e17b3dfd
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-03-26 14:58:04 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/** op-unit-translate:/index.php
+ *
+ * @created    2023-03-26
+ * @version    1.0
+ * @package    op-unit-translate
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Include
+ *
+ */
+include(__DIR__.'/Translate.class.php');
