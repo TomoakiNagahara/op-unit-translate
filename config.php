c01a44e21eab61497890c4823fe61c9b3d180354
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-02-07 20:24:47 +0900

    Fix: config.php | url_translate, url_language_list

--- config.php
+++ config.php
@@ -13,4 +13,6 @@
 return [
 	'language_area_id'       => 'op_translate_language_area_id',
 	'language_code_selected' => 'op_translate_language_code_selected',
+	'url_language_list'      => 'https://api.onepiece-framework.com/i18n/translate/language/list/',
+	'url_translate'          => 'https://api.onepiece-framework.com/i18n/translate/',
 ];
